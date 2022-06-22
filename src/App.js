8285e4a05019dbc63208d97ccc24713ea1bbe95b
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-06-22 06:42:45 +0300

    grocery-bud finished

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import List from './List';
 import Alert from "./Alert";
 
 
+const getlocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list) {
+    return JSON.parse(localStorage.getItem('list'))
+  }
+  else {
+    return []
+  }
+}
+
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getlocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
@@ -18,7 +28,18 @@ function App() {
     }
     else if (name && isEditing) {
       // deal with edit
-    }
+      setList(list.map((item)=> {
+        if (item.id === editID) {
+          return { ...item, title:name } 
+        }
+        return item
+      })
+    )
+    setName('');
+    setEditID(null);
+    setIsEditing(false);
+    showAlert(true, 'success', 'value changed')
+  }
     else {
       // show alert
       const newItem = {id: new Date().getTime().toString(), title: name};
@@ -31,10 +52,26 @@ function App() {
     setAlert({show, type, msg})
   }
 
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => item.id !== id));
+  }
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id)
+    setName(specificItem.title)
+  }
+  useEffect(()=> {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
+
   return (
   <section className="section-center">
     <form className="grocery-form" onSubmit={handleSubmit}>
-      {alert.show && <Alert {...alert} />}
+      {alert.show && <Alert {...alert} removeAlert={showAlert}
+      list={list} />}
       <h3>grocery bud</h3>
       <div className="form-control">
         <input 
@@ -50,7 +87,7 @@ function App() {
     </form>
     {list.length > 0 && (
     <div className="grocery-container">
-      <List items={list} />
+      <List items={list} removeItem={removeItem} editItem={editItem} />
       <button className="clear-btn">clear-items</button>
     </div>
     )}
